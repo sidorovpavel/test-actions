e3ceac4ade666c1e569594047a953fb22dab699a
Pavel Sidorov <sidorovpav@ya.ru>
2021-07-01 22:28:59 +0300

    MM-1 test

--- .github/actions/commit-list/index.js
+++ .github/actions/commit-list/index.js
@@ -3,18 +3,19 @@ const github = require("@actions/github");
 
 async function run() {
 	try {
-		const issueTitle = core.getInput("issue-title");
-		const jokeBody = core.getInput("joke");
-		const token = core.getInput("repo-token");
-
-		const octokit = github.getOctokit(token);
-
-		const newIssue = await octokit.issues.create({
-			repo: github.context.repo.repo,
-			owner: github.context.repo.owner,
-			title: issueTitle,
-			body: jokeBody
-		});
+		const test = core.getInput("test");
+		console.log(test);
+		// const jokeBody = core.getInput("joke");
+		// const token = core.getInput("repo-token");
+		//
+		// const octokit = github.getOctokit(token);
+		//
+		// const newIssue = await octokit.issues.create({
+		// 	repo: github.context.repo.repo,
+		// 	owner: github.context.repo.owner,
+		// 	title: issueTitle,
+		// 	body: jokeBody
+		// });
 	} catch (err) {
 		core.setFailed(err.message);
 	}
