82e7d9069a19853c1afbb488c3b42e02c34ca2b0
Pavel Sidorov <sidorovpav@ya.ru>
2021-07-04 15:21:20 +0300

    test

--- .github/actions/commit-list/issue.js
+++ .github/actions/commit-list/issue.js
@@ -7,6 +7,9 @@ async function getIssue(domain, id) {
 			headers: {
 				Accept: "application/json",
 			},
+			authorization: {
+				Basic: btoa('psidorov@tangim.com' + ":" + 'G9BzF6RS23GieYa4Afca0987'),
+			},
 		json: true
 	});
 	return res;
