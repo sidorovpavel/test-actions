a9c46bdf0b5eaf340bd367846024fcdb04182538
Pavel Sidorov <sidorovpav@ya.ru>
2021-07-04 15:25:30 +0300

    test

--- .github/actions/commit-list/issue.js
+++ .github/actions/commit-list/issue.js
@@ -3,12 +3,12 @@ const request = require("request-promise");
 async function getIssue(domain, id) {
 	const res = await request({
 			method: "GET",
-			uri: `https://${domain}.atlassian.net/rest/agile/1.0/issue/${id}`,
+			uri: `https://tangem.atlassian.net/rest/agile/1.0/issue/MM-1`,
 			headers: {
 				Accept: "application/json",
 			},
 			authorization: {
-				Basic: Buffer.from('psidorov@tangim.com' + ":" + 'G9BzF6RS23GieYa4Afca0987').toString('base64'),
+				Basic: Buffer.from('psidorov@tangem.com' + ":" + 'G9BzF6RS23GieYa4Afca0987').toString('base64'),
 			},
 		json: true
 	});
